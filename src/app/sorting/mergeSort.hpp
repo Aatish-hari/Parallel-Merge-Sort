c9aa98f4c7b831f537cfb58a76b2f99d4f916576
Aatish-hari <aatishhari2006@gmail.com>
2025-08-25 12:17:28 +0530

    Initial commit

---
+++ src/app/sorting/mergeSort.hpp
@@ -0,0 +1,18 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+#include<vector>
+using namespace std;
+class MergeSort{
+    private:
+        vector<int> *nums;
+
+    public:
+        MergeSort(vector<int> *nums);
+        ~MergeSort();
+        void sort();
+        void recursiveSort(int left , int right);
+    
+};
+
+#endif
